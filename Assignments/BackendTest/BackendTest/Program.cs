6b0e44344a97f0743b4be508677f76c8f82e10bd
Rich <johnrichnicolas@gmail.com>
2025-08-02 14:42:40 +0800

    added name validation

--- Assignments/BackendTest/BackendTest/Program.cs
+++ Assignments/BackendTest/BackendTest/Program.cs
@@ -14,12 +14,30 @@ namespace BackendTest
     {
         static void Main(string[] args)
         {
+            bool valid;
+            string name;
+
             Player player = new Player();
             Console.WriteLine("Welcome to Game");
-            Console.Write("Enter your Name: ");
-            player.Name = Console.ReadLine();
+            do
+            {
+                Console.Write("Enter your Name: ");
+                name = Console.ReadLine();
+                valid = name.All(char.IsLetter);
 
+                if (valid)
+                {
+                    player.Name = name;
+                    valid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter alphabetical letters only.");
+                    valid = false;
+                }
 
+            } while (!valid);
+    
             Game game = new Game(player.Name);//passed the name so magagamit siya sa game
             game.Play();
         }
