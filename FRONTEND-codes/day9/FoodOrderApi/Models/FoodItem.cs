e47496e967a894a7383e1848046e45ef126070b5
Rich <johnrichnicolas@gmail.com>
2025-08-08 22:32:01 +0800

    started adding files

---
+++ FRONTEND-codes/day9/FoodOrderApi/Models/FoodItem.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodOrderApi.Models
+{
+    public class FoodItem
+    {
+        public int Id { get; set; };
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
---
+++ FRONTEND-codes/day9/FoodOrderApi/Models/Order.cs
@@ -0,0 +1,8 @@
+﻿namespace FoodOrderApi.Models
+{
+    public class Order
+    {
+        public int Id { get; set; };
+        public string CustomerName { get; set; } = string.Empty;
+        public decimal Price {get; set;}
+}
