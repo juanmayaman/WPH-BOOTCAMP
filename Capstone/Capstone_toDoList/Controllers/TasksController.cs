cbe35978a40af3e07fb04abea5ff7fa5d57e0aa3
Rich <johnrichnicolas@gmail.com>
2025-08-17 02:40:00 +0800

    added the logic for deleting and completing

--- Capstone/Capstone_toDoList/Controllers/TasksController.cs
+++ Capstone/Capstone_toDoList/Controllers/TasksController.cs
@@ -8,12 +8,26 @@ namespace Capstone_toDoList.Controllers
     {
 
         private static List<TaskItem> tasks = new List<TaskItem>();
+        private static List<TaskItem> completed = new List<TaskItem>();
+        private static List<TaskItem> deleted = new List<TaskItem>();
 
         // Show task list
         public IActionResult TasksLists()
         {
             return View(tasks); // pass tasks to view
         }
+
+        public IActionResult Completed()
+        {
+            return View(completed); // pass completed tasks to view
+        }
+
+        public IActionResult Deleted()
+        {
+            return View(deleted); // pass deleted to view
+        }
+
+
 
         // Add task
         [HttpPost]
@@ -29,13 +43,7 @@ namespace Capstone_toDoList.Controllers
             task.IsCompleted = false;
             task.DateStarted = DateTime.Today; //started date
 
-            /*
-                Critical-same day,1 day duedate
-                High - 2-4days from now
-                Medium - 5 -8 days
-                Low - 9 days above
-             */
-
+            //priority
             var daysLeft = (task.DueDate - DateTime.Today).Days;
             if (daysLeft <= 1 && daysLeft >= 0) task.Priority = "Critical";
             else if (daysLeft <= 4) task.Priority = "High";
@@ -45,6 +53,45 @@ namespace Capstone_toDoList.Controllers
             tasks.Add(task);
 
             return RedirectToAction("TasksLists"); 
+        }
+
+        [HttpPost]
+        public IActionResult CompleteTask(int taskId)
+        {
+            // Find the task by Id
+            var task = tasks.FirstOrDefault(t => t.Id == taskId);
+
+            // Mark as completed
+            task.IsCompleted = true;
+            task.DateCompleted = DateTime.Now;
+
+            tasks.Remove(task);
+            completed.Add(task);
+
+            // Redirect to Tasks list or Completed tasks view
+            return RedirectToAction("Completed");
+        }
+
+        [HttpPost]
+        public IActionResult DeleteTask(int taskId)
+        {
+            // Find the task
+            var task = tasks.FirstOrDefault(t => t.Id == taskId);
+            if (task == null)
+            {
+                TempData["Error"] = "Task not found.";
+                return RedirectToAction("TasksLists");
+            }
+
+            // Set deleted date
+            task.DateDeleted = DateTime.Now;
+
+            // Move to deleted list
+            tasks.Remove(task);
+            deleted.Add(task);
+
+            // Redirect to Deleted tasks view
+            return RedirectToAction("Deleted");
         }
 
     }
