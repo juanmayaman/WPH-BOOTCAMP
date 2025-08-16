24f4fc2441d4d5b1c3a1469f0d53859585e25ae6
Rich <johnrichnicolas@gmail.com>
2025-08-17 00:02:45 +0800

    fixed the content of cards

--- Capstone/Capstone_toDoList/Controllers/TasksController.cs
+++ Capstone/Capstone_toDoList/Controllers/TasksController.cs
@@ -1,21 +1,47 @@
-﻿using System.Diagnostics;
-using Capstone_toDoList.Models;
+﻿using Capstone_toDoList.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Capstone_toDoList.Controllers
 {
     public class TasksController : Controller
     {
-        private static List<TaskItem> tasks = new List<TaskItem>(); //lists
+
+        private static List<TaskItem> tasks = new List<TaskItem>();
+
+        // Show task list
         public IActionResult TasksLists()
         {
-            return View(); 
+            return View(tasks); // pass tasks to view
         }
 
-        [HttpGet]
-        public IActionResult AddTask()
+        // Add task
+        [HttpPost]
+        public IActionResult AddTask(TaskItem task)
         {
-            return View();
+            task.Id = tasks.Count + 1;
+            task.IsCompleted = false;
+
+            //need calculate yung priority level
+            /*
+                Critical-same day,1 day duedate
+                High - 2-4days from now
+                Medium - 5 -8 days
+                Low - 9 days above
+             */
+            //for due date validation?
+            /*if (task.DueDate < DateTime.Today)
+            {
+                ModelState.AddModelError("DueDate", "Due date cannot be in the past.");
+                return View(task);
+            }*/
+
+
+            tasks.Add(task);
+
+            return RedirectToAction("TasksLists"); 
         }
+
+
     }
 }
